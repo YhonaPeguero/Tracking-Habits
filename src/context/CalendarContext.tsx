c8063cdf51772471eaf03d5fcbd330ea52fcc02c
YhonaPeguero <2017-0516@unad.edu.do>
2023-09-29 19:56:24 -0400

    add localStorage

--- src/context/CalendarContext.tsx
+++ src/context/CalendarContext.tsx
@@ -1,6 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { DayStatus } from '../types';
-
 
 type CalendarContextType = {
   days: DayStatus[];
@@ -9,49 +8,49 @@ type CalendarContextType = {
   setCurrentMonth: (value: number) => void;
 };
 
-
 export const CalendarContext = createContext<CalendarContextType | undefined>(undefined);
 
-
 export const CalendarProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  // eslint-disable-next-line no-empty-pattern
-  const [] = useState<DayStatus[]>(Array(30).fill('none'));
+  
+  const initialMonthData = { monthNumber: 1, days: Array(30).fill('none') };
+  
+  // Initialize monthsData from localStorage or use default values
+  const [monthsData, setMonthsData] = useState(() => {
+    const savedData = localStorage.getItem('monthsData');
+    return savedData ? JSON.parse(savedData) : [{ ...initialMonthData }];
+  });
+  
+  // Save to localStorage whenever monthsData changes
+  useEffect(() => {
+    localStorage.setItem('monthsData', JSON.stringify(monthsData));
+  }, [monthsData]);
+  
+  const [currentMonth, setCurrentMonth] = useState(1);
 
   const setDayStatus = (index: number, status: DayStatus) => {
-    
     const currentMonthData = monthsData[currentMonth - 1];
     const updatedDays = [...currentMonthData.days];
     updatedDays[index] = status;
-  
+
     const updatedMonthsData = [...monthsData];
     updatedMonthsData[currentMonth - 1] = {
       ...currentMonthData,
       days: updatedDays,
     };
-  
+
     setMonthsData(updatedMonthsData);
   };
 
   const handleSetCurrentMonth = (newMonth: number) => {
     if (!monthsData[newMonth - 1]) {
-      setMonthsData(prev => [...prev, { monthNumber: newMonth, days: Array(30).fill('none') }]);
+      setMonthsData((prev: Array<{ monthNumber: number, days: DayStatus[] }>) => [...prev, { monthNumber: newMonth, days: Array(30).fill('none') }]);
     }
     setCurrentMonth(newMonth);
   };
-  
-  
-
-  const [currentMonth, setCurrentMonth] = useState(1);
-  const initialMonthData = { monthNumber: 1, days: Array(30).fill('none') };
-  const [monthsData, setMonthsData] = useState([{ ...initialMonthData }]);
-
 
   return (
     <CalendarContext.Provider value={{ days: monthsData[currentMonth - 1].days, setDayStatus, currentMonth, setCurrentMonth: handleSetCurrentMonth }}>
       {children}
     </CalendarContext.Provider>
   );
-  
-  
 };
-
