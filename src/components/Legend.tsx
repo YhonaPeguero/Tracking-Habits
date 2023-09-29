a4446d057228bdf812d9c93512516340a6582224
YhonaPeguero <2017-0516@unad.edu.do>
2023-09-29 19:42:26 -0400

    update spacing in legend

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -1,18 +1,17 @@
 import { useCalendar } from '../context/useCalendar';
 
-
 const Legend = () => {
   const { days } = useCalendar();
   const completed = days.filter(day => day !== 'none').length;
 
   return (
-    <div className="flex justify-between items-center mb-6 w-full">
-      <div className="flex space-x-4">
-        <div className="flex items-center space-x-2">
+    <div className="flex justify-center items-center mb-6 w-full space-x-32">
+      <div className="flex space-x-2 md:space-x-3 lg:space-x-4">
+        <div className="flex items-center space-x-1 md:space-x-1.5 lg:space-x-2">
           <div className="w-6 h-6 bg-green-500 rounded-full"></div>
           <span className="text-white">Success</span>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-1 md:space-x-1.5 lg:space-x-2">
           <div className="w-6 h-6 bg-red-500 rounded-full"></div>
           <span className="text-white">Failure</span>
         </div>
